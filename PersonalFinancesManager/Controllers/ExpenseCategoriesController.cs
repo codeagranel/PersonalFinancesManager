61591bede9e9a01bc637d8e10001537d24268c16
Juliano Souza <juliano.sdsouza@gmail.com>
2013-02-10 13:11:39 -0200

    Atualização da interface
    Agora as páginas são acessíveis da home.

--- PersonalFinancesManager/Controllers/ExpenseCategoriesController.cs
+++ PersonalFinancesManager/Controllers/ExpenseCategoriesController.cs
@@ -1,65 +1,16 @@
-﻿using PersonalFinancesManager.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
 namespace PersonalFinancesManager.Controllers
 {
-    public class ExpenseCategoriesController : ApiController
+    public class ExpenseCategoriesController : Controller
     {
-        MainContext Context = new MainContext();
-
-        public ExpenseCategoriesController()
+        public ActionResult Index()
         {
-        }
-
-        public IEnumerable<ExpenseCategory> GetAllExpenses()
-        {
-            return Context.ExpenseCategories.AsEnumerable();
-        }
-
-        public ExpenseCategory GetExpense(int id)
-        {
-            return Context.ExpenseCategories.FirstOrDefault(x => x.Id.Equals(id));
-        }
-
-        public HttpResponseMessage PostExpense(ExpenseCategory item)
-        {
-            item = Context.ExpenseCategories.Add(item);
-            Context.SaveChanges();
-            var response = Request.CreateResponse<ExpenseCategory>(HttpStatusCode.Created, item);
-
-            string uri = Url.Link("DefaultApi", new { id = item.Id });
-            response.Headers.Location = new Uri(uri);
-            return response;
-        }
-
-        public void PutExpense(int id, ExpenseCategory item)
-        {
-            var expenseCategory = Context.ExpenseCategories.FirstOrDefault(x => x.Id.Equals(id));
-            if (item == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            expenseCategory.Name = item.Name;
-            expenseCategory.Description = item.Description;
-
-            Context.SaveChanges();
-        }
-
-        public void DeleteExpense(int id)
-        {
-            ExpenseCategory item = Context.ExpenseCategories.FirstOrDefault(x => x.Id.Equals(id));
-            if (item == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            Context.ExpenseCategories.Remove(item);
+            return View();
         }
     }
 }
--- PersonalFinancesManager/Controllers/api/MethodsOfPaymentController.cs
+++ PersonalFinancesManager/Controllers/api/MethodsOfPaymentController.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace PersonalFinancesManager.Controllers
+namespace PersonalFinancesManager.Controllers.Api
 {
     public class MethodsOfPaymentController : ApiController
     {
