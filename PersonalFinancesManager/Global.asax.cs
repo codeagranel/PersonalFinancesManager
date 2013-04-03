25bb18a328dafb5c45173f3b7517516e912d0de0
Juliano Souza <juliano.sdsouza@gmail.com>
2013-04-03 10:48:53 -0300

    Revert "Início da implementação da página de DashBoard"
    This reverts commit 74a4ebc7328c1c796956b148560acb626ce2fe5d.
    
    	deleted:    PersonalFinancesManager/Controllers/DashboardController.cs
    	modified:   PersonalFinancesManager/Global.asax.cs
    	modified:   PersonalFinancesManager/PersonalFinancesManager.csproj
    	deleted:    PersonalFinancesManager/Views/Dashboard/ExpensesByCategory.cshtml
    	deleted:    PersonalFinancesManager/Views/Dashboard/ExpensesByDate.cshtml
    	deleted:    PersonalFinancesManager/Views/Dashboard/Index.cshtml
    	modified:   PersonalFinancesManager/Views/Shared/_Layout.cshtml
    	modified:   PersonalFinancesManager/Web.config

--- PersonalFinancesManager/Global.asax.cs
+++ PersonalFinancesManager/Global.asax.cs
@@ -8,7 +8,6 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using WebMatrix.WebData;
 
 namespace PersonalFinancesManager
 {
@@ -20,18 +19,11 @@ namespace PersonalFinancesManager
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            Database.SetInitializer<MainContext>(null);
+
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            WebSecurity.InitializeDatabaseConnection(
-                  connectionStringName: "DefaultConnection",
-                  userTableName: "UserProfile",
-                  userIdColumn: "UserID",
-                  userNameColumn: "UserName",
-                  autoCreateTables: true);
         }
     }
 }
